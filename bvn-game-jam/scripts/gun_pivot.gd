6006e6bbdb3c6a3891b252a971beb285496bbf61
ExistantHumanX3 <existanthumanofficial@gmail.com>
2025-12-21 11:54:06 -0600

    Added cooldown to shooting
    Added .3 second cooldown after shooting where you can't shoot anymore

--- bvn-game-jam/scripts/gun_pivot.gd
+++ bvn-game-jam/scripts/gun_pivot.gd
@@ -2,15 +2,19 @@ extends Node2D
 
 @onready var sprite: Sprite2D = $Gun/Sprite2D
 @onready var shaders: CanvasLayer = $"../Camera/Shaders"
+@onready var cooldown: Timer = $Gun/Cooldown
 
+var canShoot = true;
 
 @export var Bullet : PackedScene = preload("res://prefabs/player_bullet.tscn")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	look_at(get_global_mouse_position())
-	if Input.is_action_just_pressed("shoot"):
+	if Input.is_action_just_pressed("shoot") && canShoot:
 		shoot()
+		canShoot = false
+		cooldown.start()
 
 
 func shoot():
@@ -22,3 +26,8 @@ func shoot():
 	
 	var shake_layer = get_tree().get_first_node_in_group("screen_shake")
 	shake_layer.start_shake(0.2)
+
+
+
+func _on_cooldown_end() -> void:
+	canShoot = true;
