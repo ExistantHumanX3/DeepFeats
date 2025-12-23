432d6ee0a1966a86123ddc21b6ceb6e8c7aae89a
nubnt <yaopan0256@gmail.com>
2025-12-23 16:00:45 -0600

    Update gun_pivot.gd

--- bvn-game-jam/scripts/gun_pivot.gd
+++ bvn-game-jam/scripts/gun_pivot.gd
@@ -13,12 +13,14 @@ const recoilStrength = 100
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	assert(game.useMouse, "for some reason game.useMouse is false???")
 	if game.useMouse:
 		look_at(get_global_mouse_position()) # TODO: this broke for some reason please fix it
 	else:
 		var input_dir = Input.get_vector("aim_left", "aim_right", "aim_up", "aim_down")
 		var look_dir = input_dir + global_position
 		look_at(look_dir)
+	
 	if Input.is_action_just_pressed("shoot") && canShoot:
 		shoot()
 		canShoot = false
